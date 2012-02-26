30ddbe785b9e5ee1d8c47a2c64e6a39c60d9a8b3
wilkie <wilkie05@gmail.com>
2012-02-26 14:34:15 -0500

    Tears out logic into separate files.

---
+++ config.h
@@ -0,0 +1,214 @@
+#include "harness.h"
+
+void do_before() {}
+void do_after()  {}
+
+struct TestDetails {
+  void  (*test_func)();
+  void  (*before_func)();
+  void  (*after_func)();
+  const char* name;
+
+  const char* filename;
+  int   line;
+
+  NewDescribeBase* parent;
+};
+
+void NewDescribeBase::addTest(void (*test_func)(), void(*before_func)(), void(*after_func)(), const char* str, const char* filename, int line) {
+  if (_tests == NULL) {
+    _tests = new TestDetails[_testMax];
+  }
+  if (_testCount == _testMax) {
+    _testMax *= 2;
+    TestDetails* old = _tests;
+    _tests = new TestDetails[_testMax];
+    memcpy(_tests, old, sizeof(TestDetails) * _testCount);
+    delete [] old;
+  }
+  _tests[_testCount].test_func = test_func;
+  _tests[_testCount].name = str;
+  _tests[_testCount].before_func = before_func;
+  _tests[_testCount].after_func = after_func;
+  _tests[_testCount].line = line;
+  _tests[_testCount].filename = filename;
+  _tests[_testCount].parent = this;
+  _testCount++;
+}
+unsigned int NewDescribeBase::totalPasses = 0;
+unsigned int NewDescribeBase::totalFails  = 0;
+
+unsigned int NewDescribeBase::_failedMax = 0;
+TestDetails* NewDescribeBase::_failedTests = NULL;
+
+NewDescribeBase::NewDescribeBase(NewDescribeBase* parent, const char* name) {
+  base = 1;
+  this->parent = parent;
+  this->name = name;
+  this->_testCount = 0;
+  this->_testMax = 32;
+  this->_tests = NULL;
+  this->_childCount = 0;
+  this->_childMax = 32;
+  this->_children = NULL;
+}
+
+void NewDescribeBase::addDescribe(NewDescribeBase* nd) {
+  if (_children == NULL) {
+    _children = new NewDescribeBase*[_childMax];
+  }
+  if (_childCount == _childMax) {
+    _childMax *= 2;
+    NewDescribeBase** old = _children;
+    _children = new NewDescribeBase*[_childMax];
+    memcpy(_children, old, sizeof(NewDescribeBase*) * _childCount);
+    delete [] old;
+  }
+  _children[_childCount] = nd;
+  _childCount++;
+}
+
+void NewDescribeBase::pass() {
+  printf(".");
+  passes++;
+  totalPasses++;
+}
+
+void NewDescribeBase::fail() {
+  printf("F");
+  if (_failedTests == NULL) {
+    _failedTests = new TestDetails[_failedMax];
+  }
+  if (totalFails == _failedMax) {
+    _failedMax *= 2;
+    TestDetails* old = _failedTests;
+    _failedTests = new TestDetails[_failedMax];
+    memcpy(_failedTests, old, sizeof(TestDetails) * totalFails);
+    delete [] old;
+  }
+  _failedTests[totalFails] = _tests[_currentTest];
+
+  fails++;
+  totalFails++;
+}
+
+void NewDescribeBase::runTests() {
+  // Run each child describe
+  for(unsigned int i = 0; i < _childCount; i++) {
+    NewDescribeBase* base = _children[i];
+    base->runTests();
+  }
+
+  // Run each test
+  for(unsigned int i = 0; i < _testCount; i++) {
+    _currentTest = i;
+    _tests[i].before_func();
+    _tests[i].test_func();
+    _tests[i].after_func();
+  }
+}
+
+NewDescribe::NewDescribe(const char* name) : NewDescribeBase(NULL, name) {
+}
+
+NewTest::NewTest(NewDescribeBase* parent, void(*before_func)(), void(*after_func)(), void(*test_func)(), const char* str, const char* filename, int line) {
+  parent->addTest(test_func, before_func, after_func, str, filename, line);
+}
+
+NewDescribe _describe("{}");
+
+char* yieldDescribeName(NewDescribeBase* ds) {
+  NewDescribeBase* current = ds;
+  int maxChars = 0;
+  while(current != NULL) {
+    maxChars += strlen(current->name) + 2;
+    current = current->parent;
+  }
+  char* testRoot = new char[maxChars+1];
+  char* origRoot = testRoot;
+  strcpy(testRoot, "");
+
+  current = ds;
+  while(current != NULL) {
+    // Shift array over current->name + 2
+    // If it is not the root
+    if (strcmp(current->name, "{}") != 0) {
+      int shiftAmount = strlen(current->name) + 2;
+      for (int i = shiftAmount + strlen(testRoot) + 1; i >= shiftAmount; i--) {
+        testRoot[i] = testRoot[i - shiftAmount];
+      }
+      // Copy (without \0) over string
+      for (int i = 0; i < strlen(current->name); i++) {
+        testRoot[i] = current->name[i];
+      }
+      testRoot[shiftAmount-1] = ':';
+      testRoot[shiftAmount-2] = ':';
+    }
+    current = current->parent;
+  }
+
+  // Remove last ::
+  testRoot[strlen(testRoot)-2] = '\0';
+
+  return testRoot;
+}
+
+void printFailure(TestDetails test) {
+  char* describeName = yieldDescribeName(test.parent);
+
+  printf("Failed: %s:%d %s %s\n", test.filename, test.line, describeName, test.name);
+
+  delete [] describeName;
+}
+
+void printDescribe(NewDescribeBase* root) {
+  char* describeName = yieldDescribeName(root);
+
+  if (strlen(describeName) > 0) {
+    printf("%s\n", describeName);
+
+    for(unsigned int t = 0; t < root->_testCount; t++) {
+      printf("  %s\n", root->_tests[t].name);
+    }
+  }
+
+  delete [] describeName;
+
+  printf("\n");
+
+  for(unsigned int c = 0; c < root->_childCount; c++) {
+    printDescribe(root->_children[c]);
+  }
+}
+
+void printDocumentation() {
+  printDescribe(&_describe);
+}
+
+void reportTests() {
+  printf("\n");
+  printf("Passes: %d\n", _describe.totalPasses);
+  printf("Failures: %d\n", _describe.totalFails);
+  printf("\n");
+
+  for(unsigned int i = 0; i < _describe.totalFails; i++) {
+    printFailure(_describe._failedTests[i]);
+  }
+}
+
+int main(int argc, char** argv) {
+  if (argc > 1) {
+    if (strncmp(argv[1], "--docs", 6) == 0) {
+      printDocumentation();
+    }
+    else {
+      printf("Unknown arguments\n");
+    }
+    return 0;
+  }
+
+  _describe.runTests();
+  reportTests();
+
+  return 0;
+}
--- harness.h
+++ harness.h
@@ -6,109 +6,17 @@
 
 typedef void (*Test)();
 
-class NewDescribeBase;
-
-struct TestDetails {
-  void  (*test)();
-  void  (*before)();
-  void  (*after)();
-  const char* name;
-
-  const char* filename;
-  int   line;
-
-  NewDescribeBase* parent;
-};
+struct TestDetails;
 
 class NewDescribeBase {
   public:
-    NewDescribeBase(NewDescribeBase* parent, const char* name) {
-      base = 1;
-      this->parent = parent;
-      this->name = name;
-      this->_testCount = 0;
-      this->_testMax = 32;
-      this->_tests = NULL;
-      this->_childCount = 0;
-      this->_childMax = 32;
-      this->_children = NULL;
-    }
+    NewDescribeBase(NewDescribeBase* parent, const char* name);
+    void addDescribe(NewDescribeBase* nd);
+    void addTest(void (*test)(), void(*before)(), void(*after)(), const char* str, const char* filename, int line);
+    void pass();
+    void fail();
+    void runTests();
 
-    void addDescribe(NewDescribeBase* nd) {
-      if (_children == NULL) {
-        _children = new NewDescribeBase*[_childMax];
-      }
-      if (_childCount == _childMax) {
-        _childMax *= 2;
-        NewDescribeBase** old = _children;
-        _children = new NewDescribeBase*[_childMax];
-        memcpy(_children, old, sizeof(NewDescribeBase*) * _childCount);
-        delete [] old;
-      }
-      _children[_childCount] = nd;
-      _childCount++;
-    }
-
-    void addTest(void (*test)(), void(*before)(), void(*after)(), const char* str, const char* filename, int line) {
-      if (_tests == NULL) {
-        _tests = new TestDetails[_testMax];
-      }
-      if (_testCount == _testMax) {
-        _testMax *= 2;
-        TestDetails* old = _tests;
-        _tests = new TestDetails[_testMax];
-        memcpy(_tests, old, sizeof(TestDetails) * _testCount);
-        delete [] old;
-      }
-      _tests[_testCount].test = test;
-      _tests[_testCount].name = str;
-      _tests[_testCount].before = before;
-      _tests[_testCount].after = after;
-      _tests[_testCount].line = line;
-      _tests[_testCount].filename = filename;
-      _tests[_testCount].parent = this;
-      _testCount++;
-    }
-
-    void pass() {
-      printf(".");
-      passes++;
-      totalPasses++;
-    }
-
-    void fail() {
-      printf("F");
-      if (_failedTests == NULL) {
-        _failedTests = new TestDetails[_failedMax];
-      }
-      if (totalFails == _failedMax) {
-        _failedMax *= 2;
-        TestDetails* old = _failedTests;
-        _failedTests = new TestDetails[_failedMax];
-        memcpy(_failedTests, old, sizeof(TestDetails) * totalFails);
-        delete [] old;
-      }
-      _failedTests[totalFails] = _tests[_currentTest];
-
-      fails++;
-      totalFails++;
-    }
-
-    void runTests() {
-      // Run each child describe
-      for(unsigned int i = 0; i < _childCount; i++) {
-        NewDescribeBase* base = _children[i];
-        base->runTests();
-      }
-
-      // Run each test
-      for(unsigned int i = 0; i < _testCount; i++) {
-        _currentTest = i;
-        _tests[i].before();
-        _tests[i].test();
-        _tests[i].after();
-      }
-    }
     int base;
     const char* name;
     NewDescribeBase* parent;
@@ -125,29 +33,20 @@ class NewDescribeBase {
     static TestDetails* _failedTests;
 };
 
-void do_before() {}
-void do_after()  {}
-
-unsigned int NewDescribeBase::totalPasses = 0;
-unsigned int NewDescribeBase::totalFails  = 0;
-
-unsigned int NewDescribeBase::_failedMax = 0;
-TestDetails* NewDescribeBase::_failedTests = NULL;
+void do_before();
+void do_after();
 
 class NewDescribe : public NewDescribeBase {
   public:
-    NewDescribe(const char* name) : NewDescribeBase(NULL, name) {
-    }
+    NewDescribe(const char* name);
 };
 
 class NewTest {
   public:
-    NewTest(NewDescribeBase* parent, void(*before)(), void(*after)(), void(*test)(), const char* str, const char* filename, int line) {
-      parent->addTest(test, before, after, str, filename, line);
-    }
+    NewTest(NewDescribeBase* parent, void(*before)(), void(*after)(), void(*test)(), const char* str, const char* filename, int line);
 };
 
-NewDescribe describe("{}");
+extern NewDescribe _describe;
 
 // This macro will place quotes around the given token
 #define STRINGIFY_2(x) #x
@@ -167,125 +66,38 @@ NewDescribe describe("{}");
 
 #define it(str)                                                    \
   void UNIQUE_TEST(test)();                                        \
-  NewTest UNIQUE_TESTER(test)(&describe, &do_before, &do_after, &UNIQUE_TEST(test), str, __FILE__, __LINE__); \
+  NewTest UNIQUE_TESTER(test)(&_describe, &do_before, &do_after, &UNIQUE_TEST(test), str, __FILE__, __LINE__); \
   void UNIQUE_TEST(test)()
 
 #define should(expr)   \
   if (expr) {          \
-	  describe.pass(); \
+	  _describe.pass();  \
   }                    \
   else {               \
-    describe.fail();   \
+    _describe.fail();  \
     return;            \
   } do {} while(0)
 
-#define describe(x)                                                        \
-  namespace Describe##x {                                                  \
-    class NewDescribeBaseA : public NewDescribeBase {                      \
-      public:                                                              \
-        NewDescribeBaseA(::NewDescribeBase* parent, const char* name)      \
-           : NewDescribeBase(parent, name) {}                              \
-    };                                                                     \
-    class NewDescribeBase : public NewDescribeBaseA {                      \
-      public:                                                              \
-        NewDescribeBase(::NewDescribeBase* parent, const char* name)       \
-           : NewDescribeBaseA(parent, name) { base++; }                    \
-    };                                                                     \
-    class NewDescribe : public NewDescribeBase {                           \
-      public:                                                              \
-        NewDescribe(const char* name) : NewDescribeBase(&describe, name) { \
-          parent->addDescribe(this);                                       \
-        }                                                                  \
-    };                                                                     \
-    NewDescribe describe(STRINGIFY(x));                                    \
-  }                                                                        \
-  namespace Describe##x
-
-char* yieldDescribeName(NewDescribeBase* describe) {
-  NewDescribeBase* current = describe;
-  int maxChars = 0;
-  while(current != NULL) {
-    maxChars += strlen(current->name) + 2;
-    current = current->parent;
-  }
-  char* testRoot = new char[maxChars+1];
-  char* origRoot = testRoot;
-  strcpy(testRoot, "");
-
-  current = describe;
-  while(current != NULL) {
-    // Shift array over current->name + 2
-    // If it is not the root
-    if (strcmp(current->name, "{}") != 0) {
-      int shiftAmount = strlen(current->name) + 2;
-      for (int i = shiftAmount + strlen(testRoot) + 1; i >= shiftAmount; i--) {
-        testRoot[i] = testRoot[i - shiftAmount];
-      }
-      // Copy (without \0) over string
-      for (int i = 0; i < strlen(current->name); i++) {
-        testRoot[i] = current->name[i];
-      }
-      testRoot[shiftAmount-1] = ':';
-      testRoot[shiftAmount-2] = ':';
-    }
-    current = current->parent;
-  }
-
-  // Remove last ::
-  testRoot[strlen(testRoot)-2] = '\0';
-
-  return testRoot;
-}
-
-void printFailure(TestDetails test) {
-  char* describeName = yieldDescribeName(test.parent);
-
-  printf("Failed: %s:%d %s %s\n", test.filename, test.line, describeName, test.name);
-
-  delete [] describeName;
-}
-
-void printDescribe(NewDescribeBase* root) {
-  char* describeName = yieldDescribeName(root);
-
-  if (strlen(describeName) > 0) {
-    printf("%s\n", describeName);
-
-    for(unsigned int t = 0; t < root->_testCount; t++) {
-      printf("  %s\n", root->_tests[t].name);
-    }
-  }
-
-  delete [] describeName;
-
-  printf("\n");
-
-  for(unsigned int c = 0; c < root->_childCount; c++) {
-    printDescribe(root->_children[c]);
-  }
-}
-
-void printDocumentation() {
-  printDescribe(&describe);
-}
-
-void reportTests() {
-  printf("\n");
-  printf("Passes: %d\n", describe.totalPasses);
-  printf("Failures: %d\n", describe.totalFails);
-  printf("\n");
-
-  for(unsigned int i = 0; i < describe.totalFails; i++) {
-    printFailure(describe._failedTests[i]);
-  }
-}
-
-int main() {
-  describe.runTests();
-
-  reportTests();
-
-  return 0;
-}
+#define describe(x)                                                         \
+  namespace Describe##x {                                                   \
+    class NewDescribeBaseA : public NewDescribeBase {                       \
+      public:                                                               \
+        NewDescribeBaseA(::NewDescribeBase* parent, const char* name)       \
+           : NewDescribeBase(parent, name) {}                               \
+    };                                                                      \
+    class NewDescribeBase : public NewDescribeBaseA {                       \
+      public:                                                               \
+        NewDescribeBase(::NewDescribeBase* parent, const char* name)        \
+           : NewDescribeBaseA(parent, name) { base++; }                     \
+    };                                                                      \
+    class NewDescribe : public NewDescribeBase {                            \
+      public:                                                               \
+        NewDescribe(const char* name) : NewDescribeBase(&_describe, name) { \
+          parent->addDescribe(this);                                        \
+        }                                                                   \
+    };                                                                      \
+    NewDescribe _describe(STRINGIFY(x));                                    \
+  }                                                                         \
+namespace Describe##x
 
 #endif
